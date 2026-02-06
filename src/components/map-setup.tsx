4af52f19806e3b997fc12e4bbbbc6394657f9dd9
Giosepe Luiz <giosepe_luiz3@live.com>
2026-02-06 23:15:16 +0000

    🐛 correção: remoção do interactive map

--- src/components/map-setup.tsx
+++ src/components/map-setup.tsx
@@ -1,15 +1,5 @@
 import React from "react";
 
-export const dynamic = "force-dynamic";
-
 export default function MapSetup() {
-  return (
-    <>
-      <link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/leaflet@1.9.4/dist/leaflet.min.css"
-      />
-      <script src="https://cdn.jsdelivr.net/npm/leaflet@1.9.4/dist/leaflet.js"></script>
-    </>
-  );
+  return null;
 }
